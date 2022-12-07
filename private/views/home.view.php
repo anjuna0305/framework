1c70a847426577cd5a2859d2526c53c746b3b93f
anjuna serasingha <anjunaserasingha@gmail.com>
2022-12-07 22:49:08 +0530

    first commit

---
+++ private/views/home.view.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../public/assets/styles/styles.css">
+    <link rel="shortcut icon" href="/favicon.ico">
+</head>
+
+<body>
+    <div class="event_item_completed">
+        <div class="event_image">
+            <img src="Photos/foodDonationEvent.jpg" alt="">
+        </div>
+
+        <h4 class="row">Completed</h4>
+        <div class="event_button_holder_completed">
+            <button class="greenButton event_button">View Details</button>
+        </div>
+    </div>
+</body>
+
+</html>
